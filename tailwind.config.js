8572697dd040d63be0436ac2f8205121671b19ed
jesus <jesussanchezz0004@gmail.com>
2025-06-11 14:09:18 +0200

    Cambios a la página con Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const {colors: defaultColors} = require('tailwindcss/defaultTheme');
+
+    module.exports = {
+        theme:  {
+            extend: {
+                colors: {
+                'custom-blue': '#5961f4',
+            }
+        },
+        },
+    };
